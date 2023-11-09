5cc99b55263d9f5640c8185ef268f9af0f1d9f82
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-11-09 15:52:40 -0300

    :feat: creating logic to make the o form work

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 import TemplateDefault from '../template/Default';
 
@@ -10,13 +11,35 @@ const Contact = () => {
    const [phone, setPhone] = useState('');
    const [message, setMessage] = useState('');
 
+   const sendEmail = (e: any) => {
+      e.preventDefault();
+
+      const templateParams = {
+         from_name: name,
+         message: message,
+         email: email,
+         phone: phone
+      }
+
+      emailjs.send('service_iaa399s', 'template_afnu03e', templateParams, 'sc392kkY-hhraF3HP')
+         .then((response) => {
+            console.log('Email enviado', response.status, response.text);
+            setName('');
+            setEmail('');
+            setPhone('');
+            setMessage('');
+         }, (err) => {
+            console.log('Failed: ', err);
+         })
+   }
+
    return(
       <TemplateDefault>
          <main className='flex justify-center py-16 w-full h-auto bg-zinc-800'>
             <div className='w-5/12'>
                <form 
                   className='flex flex-col space-y-8'
-                  onSubmit={() => {}}
+                  onSubmit={sendEmail}
                >
                   <input 
                      className='p-3 rounded-md shadow-md tracking-wider text-zinc-400 bg-zinc-900'
@@ -48,13 +71,16 @@ const Contact = () => {
 
                   </textarea>
 
-                  <button
-                     className='relative left-[71%] w-40 p-2 text-xl text-zinc-500 font-semibold rounded-md tracking-widest bg-zinc-950'
-                     type='submit'
-                     value='Enviar'
-                  >
-                     Enviar
-                  </button>
+                  <div className='w-full flex justify-end'>
+                     <button
+                        className='w-40 p-2 text-xl text-zinc-500 font-semibold rounded-md tracking-widest transition-all duration-200 bg-zinc-950 hover:text-zinc-300'
+                        type='submit'
+                        value='Enviar'
+                     >
+                        Enviar
+                     </button>
+                  </div>
+
                </form>
             </div>
          </main>
