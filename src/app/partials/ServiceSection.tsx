3b2a9a5d156da8d2d08574143c5e6569c5f033a2
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-11-17 12:02:36 -0300

    :fix: rename component service section

---
+++ src/app/partials/ServiceSection.tsx
@@ -0,0 +1,47 @@
+const ServiceSection = () => {
+   return(
+      <article className='grid grid-cols-1 lg:grid-cols-2 gap-5 w-full tracking-wider leading-7'>
+         <div className='flex flex-col items-center py-6 px-4 w-full h-52 rounded-lg shadow-lg transition-all duration-300 bg-zinc-700 hover:scale-105'>
+            <h2 className='my-3 text-xl text-zinc-300 font-semibold'>
+               Harmonização
+            </h2>
+
+            <p className='text-zinc-400'>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit velit consequuntur praesentium 
+            </p>
+         </div>
+
+         <div className='flex flex-col items-center py-6 px-4 w-full h-52 rounded-lg shadow-lg transition-all duration-300 bg-zinc-700 hover:scale-105'>
+            <h2 className='my-3 text-xl text-zinc-300 font-semibold'>
+               Harmonização
+            </h2>
+
+            <p className='text-zinc-400'>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit velit consequuntur praesentium 
+            </p>
+         </div>
+
+         <div className='flex flex-col items-center py-6 px-4 w-full h-52 rounded-lg shadow-lg transition-all duration-300 bg-zinc-700 hover:scale-105'>
+            <h2 className='my-3 text-xl text-zinc-300 font-semibold'>
+               Harmonização
+            </h2>
+
+            <p className='text-zinc-400'>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit velit consequuntur praesentium 
+            </p>
+         </div>
+
+         <div className='flex flex-col items-center py-6 px-4 w-full h-52 rounded-lg shadow-lg transition-all duration-300 bg-zinc-700 hover:scale-105'>
+            <h2 className='my-3 text-xl text-zinc-300 font-semibold'>
+               Harmonização
+            </h2>
+
+            <p className='text-zinc-400'>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit velit consequuntur praesentium 
+            </p>
+         </div>
+      </article>
+   );
+}
+
+export default ServiceSection;
