2f13944123c4165d55f531bedf83859d356ef405
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-11-17 16:15:10 -0300

    :fix: fixing responsive error

--- src/app/components/ToastConfirm.tsx
+++ src/app/components/ToastConfirm.tsx
@@ -4,11 +4,11 @@ const ToastConfirm = () => {
 
    return(
       <motion.div 
-         className='absolute top-full left-10 px-6 py-3 rounded-lg bg-green-500 shadow-md shadow-green-700 lg:left-16'
+         className='fixed top-12 left-0 px-6 py-3 rounded-lg bg-green-500 shadow-md shadow-green-700 lg:left-16'
          initial={{ left: -100 }}
          animate={{ left: 100 }}
       >
-         <p className='text-lg text-zinc-300 tracking-widest font-medium'>
+         <p className='text-lg text-zinc-100 tracking-widest font-medium'>
             Email Enviado!
          </p>
       </motion.div>
